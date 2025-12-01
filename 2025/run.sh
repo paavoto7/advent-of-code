3810c0dd17ee869400dff74b6eee627bb979a6ef
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2025-12-01 19:03:59 +0200

    Init 2025

---
+++ 2025/run.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Using a makefile would be a lot better, but this was just quickly done
+# I might add a makefile later during the days especially when I have more helpers
+
+# Makes it easier to run the programs. 
+# Run using ./run.sh DAY_NUMBER [WHATEVER_TO_USE_TEST]
+# Needs to have execute permission => chmod u+x run.sh
+
+cd "$(dirname "$0")"
+
+if [ "$1" = "clean" ]
+then
+    rm ./output/day*
+    exit 0
+fi
+
+if [ -z "$1" ]
+then
+    echo "No day provided. Provide at least ./run.sh DAY_NUMBER."
+    exit 1
+fi
+
+# Get the current day and month
+printf -v day "%(%-d)T" -1
+printf -v month "%(%-m)T" -1
+
+# If it is december, check that the supplied day is not over the current one
+if [ $month -eq 12 ]
+then
+    if [ "$1" -gt $day ]
+    then
+        echo "Day not yet available for this year"
+        exit 1
+    fi
+fi
+
+if [ ! -e "./output/helper.o" ]
+then
+    g++ "./utils/helper.cpp" "./day$1/day$1.cpp" -o "./output/day$1"
+else
+    g++ "./day$1/day$1.cpp" "./output/helper.o" -o "./output/day$1"
+fi
+
+# Checks whether compilation was succesful or not
+# $? means last commands return value, -ne is not equal
+if [ $? -ne 0 ]
+then
+    echo "Compilation of the program failed."
+    exit 1
+fi
+
+if [ -z "$2" ]
+then
+    ./output/day$1 "./inputs/day$1.txt"
+else
+    ./output/day$1 "./inputs/test.txt"
+fi
