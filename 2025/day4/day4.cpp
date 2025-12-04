3e8b714954c0695f6a3044751a002ad8108fae62
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2025-12-04 21:02:14 +0200

    2025 day 4 comments and minor tweaks

--- 2025/day4/day4.cpp
+++ 2025/day4/day4.cpp
@@ -1,14 +1,18 @@
 #include <vector>
 #include <string>
-#include <list>
 #include "../../utils/helper.h"
 #include "../../utils/timer.h"
 
 using namespace std;
 using namespace utils;
 
-// I will add reasoning/documentation in the evening
-
+/*
+This is a simple linear brute-force grid traversal solution.
+All it does is loop over each element and check it's adjacent cells.
+In the adjacency loop it breaks early if the needed 4 adjacent is reached.
+Initially I didn't have that, but upon testing it provided some
+performance improvements.
+*/
 int part1(const vector<string>& input) {
 
     constexpr int dirs[8][2] = {
@@ -16,14 +20,13 @@ int part1(const vector<string>& input) {
         {1, 1}, {-1, 1}, {-1, -1}, {1, -1},
     };
 
-    const auto rows = input.size();
-    const auto cols = input[0].size();
+    const int rows = static_cast<int>(input.size());
+    const int cols = static_cast<int>(input[0].size());
 
-    int accessableRolls = 0;
-    for (size_t i = 0; i < rows; ++i) {
-        for (size_t j = 0; j < cols; ++j) {
-            char pos = input[i][j];
-            if (pos == '.') continue;
+    int accessibleRolls = 0;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (input[i][j] == '.') continue;
 
             int adjRolls = 0;
             for (int k = 0; k < 8; ++k) {
@@ -33,15 +36,30 @@ int part1(const vector<string>& input) {
                 if (isInBounds(y, x, rows, cols) && input[y][x] == '@') {
                     adjRolls++;
                 }
+
+                if (adjRolls == 4) break;
             }
 
-            if (adjRolls < 4) accessableRolls++;
+            if (adjRolls < 4) accessibleRolls++;
         }
     }
     
-    return accessableRolls;
+    return accessibleRolls;
 }
 
+/*
+This was a pleasant part 2. The solution is largely the same as part 1.
+
+The main difference is that, besides incrementing the accessible rolls,
+it changes current to a dot (not roll) and goes back to the top-left corner
+in the eight-sized adjacency grid.
+
+It naively re-iterates all the elements on the previous level after removing one.
+Of course, this is highly inefficient, but I achieve runtimes of approximately
+5-6 ms with -O2 and 4-6 ms with -O3. Without it is ~27-28 ms.
+
+Might come back later and optimise, but I will leave it as is for now.
+*/
 int part2(vector<string> input) {
 
     constexpr int dirs[8][2] = {
@@ -49,37 +67,39 @@ int part2(vector<string> input) {
         {1, 1}, {-1, 1}, {-1, -1}, {1, -1},
     };
 
-    const auto rows = input.size();
-    const auto cols = input[0].size();
+    const int rows = static_cast<int>(input.size());
+    const int cols = static_cast<int>(input[0].size());
 
-    int accessableRolls = 0;
-    for (size_t i = 0; i < rows; ++i) {
-        for (size_t j = 0; j < cols; ++j) {
+    int accessibleRolls = 0;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
             if (input[i][j] == '.') continue;
 
             int adjRolls = 0;
 
             for (int k = 0; k < 8; ++k) {
                 // Could be without static cast, as isInBounds checks for too high values
-                int y = static_cast<int>(i) + dirs[k][0];
-                int x = static_cast<int>(j) + dirs[k][1];
+                int y = i + dirs[k][0];
+                int x = j + dirs[k][1];
 
                 if (isInBounds(y, x, rows, cols) && input[y][x] == '@') {
                     adjRolls++;
                 }
+
+                if (adjRolls == 4) break;
             }
 
             if (adjRolls < 4) {
-                accessableRolls++;
+                accessibleRolls++;
                 input[i][j] = '.';
                 // Go to the top-left corner in the eight adjacent grid
-                i -= 1 - (i == 0);
-                j -= 2 - (j == 0);
+                if (i > 0) i--;
+                j -= (j > 0) ? 2 : 1;
             }
         }
     }
     
-    return accessableRolls;
+    return accessibleRolls;
 }
 
 int main(int argc, char *argv[]) {
